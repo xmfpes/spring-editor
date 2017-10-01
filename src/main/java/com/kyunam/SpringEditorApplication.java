2c8e02a5fa2efbc7dd8ee8eb3f84b1dedc6dc028
KyuNam <xmfpes@naver.com>
2017-10-01 20:57:00 +0900

    summernote editor s3 연동 및 게시글 작성

---
+++ src/main/java/com/kyunam/SpringEditorApplication.java
@@ -0,0 +1,12 @@
+package com.kyunam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEditorApplication.class, args);
+	}
+}
